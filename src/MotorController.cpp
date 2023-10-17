5c6b4549db911a70d25fa8f7337feb45975f61b2
Orlin Dimitrov <orlin369@gmail.com>
2023-10-18 00:04:03 +0300

    Add code files

---
+++ src/MotorController.cpp
@@ -0,0 +1,222 @@
+/*
+
+MIT License
+
+Copyright (c) [2019] [Orlin Dimitrov]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#include "MotorController.h"
+
+/** @brief Initialize the H bridge for motor control.
+ *  @return Void.
+ */
+void MotorControllerClass::init(MotorModel_t* model)
+{
+	m_motorModel = *model;
+	m_cntLeft = 0;
+	m_cntRight = 0;
+	m_dirCntLeft = 0;
+	m_dirCntRight = 0;
+
+	// Setup the motor driver.
+	pinMode(m_motorModel.PinLeftForward, OUTPUT);
+	pinMode(m_motorModel.PinRightForward, OUTPUT);
+	pinMode(m_motorModel.PinLeftBackward, OUTPUT);
+	pinMode(m_motorModel.PinRightBackward, OUTPUT);
+	analogWrite(m_motorModel.PinLeftForward, 0);
+	analogWrite(m_motorModel.PinRightForward, 0);
+	analogWrite(m_motorModel.PinLeftBackward, 0);
+	analogWrite(m_motorModel.PinRightBackward, 0);
+}
+
+/** @brief Update left encoder value.
+ *  @return Void.
+ */
+void MotorControllerClass::UpdateLeftEncoder()
+{
+	if (m_dirCntLeft < 0)
+	{
+    // Decrement Motor Left counter value.
+    m_cntLeft--;
+	}
+	else if (m_dirCntLeft > 0)
+	{
+    // Increment Motor Left counter value.
+    m_cntLeft++;
+	}
+}
+
+/** @brief Update right encoder value.
+ *  @return Void.
+ */
+void MotorControllerClass::UpdateRightEncoder()
+{
+	if (m_dirCntRight < 0)
+	{
+    // Decrement motor right counter value.
+    m_cntRight--;
+	}
+	else if (m_dirCntRight > 0)
+	{
+    // Increment motor right counter value.
+    m_cntRight++;
+	}
+}
+
+// Function to convert from centimeters to steps
+unsigned int MotorControllerClass::MM2Steps(float mm)
+{
+	// Final calculation result.
+	unsigned int result;
+
+	// Calculate wheel circumference in mm.
+	float circumference = (m_motorModel.WheelDiameter * PI) / 10;
+
+	// mm per Step.
+	float mm_step = circumference / m_motorModel.EncoderTracs;
+
+	// Calculate result as a float.
+	float f_result = mm / mm_step;
+
+	// Convert to an integer (note this is NOT rounded)
+	result = (unsigned int)f_result;
+
+	// End and return result.
+	return result;
+}
+
+/** @brief Control the PWM chanels of the H bridge for motor control.
+ *  @param left int16_t, input value holding values of the left pair PWMs.
+ *  @param right int16_t, input value holding values of the right pair PWMs.
+ *  @return Void.
+ */
+void MotorControllerClass::SetPWM(int16_t left, int16_t right)
+{
+	if (left > 0)
+	{
+		// Forward.
+    m_dirCntLeft = 1;
+		analogWrite(m_motorModel.PinLeftForward, 0);
+		analogWrite(m_motorModel.PinLeftBackward, abs(left));
+	}
+	else if (left < 0)
+	{
+		// Revers.
+    m_dirCntLeft = -1;
+		analogWrite(m_motorModel.PinLeftBackward, 0);
+		analogWrite(m_motorModel.PinLeftForward, abs(left));
+	}
+	else
+	{
+    m_dirCntLeft = 0;
+		analogWrite(m_motorModel.PinLeftForward, 0);
+		analogWrite(m_motorModel.PinLeftBackward, 0);
+	}
+
+	if (right > 0)
+	{
+		// Forward.
+    m_dirCntRight = 1;
+		analogWrite(m_motorModel.PinRightBackward, 0);
+		analogWrite(m_motorModel.PinRightForward, abs(right));
+	}
+	else if (right < 0)
+	{
+		// Revers.
+    m_dirCntRight = -1;
+		analogWrite(m_motorModel.PinRightForward, 0);
+		analogWrite(m_motorModel.PinRightBackward, abs(right));
+	}
+	else
+	{
+    m_dirCntRight = 0;
+		analogWrite(m_motorModel.PinRightForward, 0);
+		analogWrite(m_motorModel.PinRightBackward, 0);
+	}
+}
+
+
+/**
+ * @brief Get the Left Encoder value.
+ * 
+ * @return unsigned int Value
+ */
+long MotorControllerClass::GetLeftEncoder()
+{
+	return this->m_cntLeft;
+}
+
+/**
+ * @brief Get the Right Encoder value.
+ * 
+ * @return unsigned int Value
+ */
+long MotorControllerClass::GetRightEncoder()
+{
+	return this->m_cntRight;
+}
+
+/**
+ * @brief Set the Left Encoder value.
+ * 
+ * @param value Value
+ */
+void MotorControllerClass::SetLeftEncoder(long value)
+{
+	this->m_cntLeft = value;
+}
+
+/**
+ * @brief Set the Right Encoder value.
+ * 
+ * @param value Value
+ */
+void MotorControllerClass::SetRightEncoder(long value)
+{
+	this->m_cntRight = value;
+}
+
+/**
+ * @brief Get the Left Motor PWM value.
+ * 
+ * @return int16_t Value
+ */
+int16_t MotorControllerClass::GetLeftMotor()
+{
+	return this->m_leftPWM;
+}
+
+/**
+ * @brief Get the Right Motor PWM value.
+ * 
+ * @return int16_t Value
+ */
+int16_t MotorControllerClass::GetRightMotor()
+{
+	return this->m_rightPWM;
+}
+
+/**
+ * @brief Bridge controller instance.
+ * 
+ */
+MotorControllerClass MotorController;
---
+++ src/MotorController.h
@@ -0,0 +1,193 @@
+/*
+
+MIT License
+
+Copyright (c) [2023] [OpenMOBot]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+// MotorController.h
+
+#ifndef _MOTORCONTROLLER_h
+#define _MOTORCONTROLLER_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+// #include "DebugPort.h"
+
+typedef struct
+{
+	uint8_t PinLeftForward; ///< Left speed pin.
+	uint8_t PinRightForward; ///< Right speed pin.
+	uint8_t PinLeftBackward; ///< Left direction pin.
+	uint8_t PinRightBackward; ///< Right direction pin.
+	double WheelDiameter; ///< Wheels diameter.
+	double DistanceBetweenWheels; ///< Distance between wheels.
+	uint32_t EncoderTracs; ///< Number of encoders track.
+/** @brief H-bridge motor Controller. */
+} MotorModel_t;
+
+/** @brief H-bridge motor hontroller. */
+class MotorControllerClass
+{
+
+ protected:
+
+#pragma region Variables
+
+	 /** @brief Motor settings. */
+	 MotorModel_t m_motorModel;
+
+	 /** @brief Left encoder counter. */
+	 volatile long m_cntLeft;
+
+	 /** @brief Right encoder counter. */
+	 volatile long m_cntRight;
+
+	 /** @brief Left encoder counter enable flag. */
+	 volatile int8_t m_dirCntLeft;
+
+	 /** @brief Right encoder counter enable flag. */
+	 volatile int8_t m_dirCntRight;
+
+	/**
+	 * @brief Left motor PWM value.
+	 * 
+	 */
+	int16_t m_leftPWM;
+
+	/**
+	 * @brief Right motor PWM value.
+	 * 
+	 */
+	int16_t m_rightPWM;
+
+#pragma endregion
+
+#pragma region Methods
+
+	/** @brief Function to convert from millimeters to steps.
+	 *  @param mm float, Millimeters distance.
+	 *  @return encoder counts.
+	 */
+	unsigned int MM2Steps(float mm);
+
+#pragma endregion
+
+ public:
+
+#pragma region Methods
+
+	/** @brief Initialize the bridge controller.
+	 *  @param model MotorModel_t, Motor the controller.
+	 *  @return Void.
+	 */
+	void init(MotorModel_t* model);
+
+	/** @brief Incremet the left encoder value.
+	 *  @return Void.
+	 */
+	void UpdateLeftEncoder();
+
+	/** @brief Incremet the right encoder value.
+	 *  @return Void.
+	 */
+	void UpdateRightEncoder();
+
+	/** @brief Function to Move Forward/Backwards.
+	 *  @param mm float, Millimeters to be done.
+	 *  @param mspeed int, input value holding values of the PWM.
+	 *  @return Void.
+	 */
+	void MoveMM(float mm, int mspeed);
+
+	/** @brief Function to Spin Right.
+	 *  @param mm float, Millimeters to be done.
+	 *  @param mspeed int, input value holding values of the PWM.
+	 *  @return Void.
+	 */
+	void SpinRight(float mm, int mspeed);
+
+	/** @brief Function to Spin Left.
+	 *  @param mm float, Millimeters to be done.
+	 *  @param mspeed int, input value holding values of the PWM.
+	 *  @return Void.
+	 */
+	void SpinLeft(float mm, int mspeed);
+
+	void MoveSpeed(int16_t left, int16_t right);
+	void SetPWM(int16_t left, int16_t right);
+
+	/**
+	 * @brief Get the Left Encoder value.
+	 * 
+	 * @return unsigned int Value
+	 */
+	long GetLeftEncoder();
+
+	/**
+	 * @brief Get the Right Encoder value.
+	 * 
+	 * @return unsigned int Value
+	 */
+	long GetRightEncoder();
+
+	/**
+	 * @brief Set the Left Encoder value.
+	 * 
+	 * @param value Value
+	 */
+	void SetLeftEncoder(long value);
+
+	/**
+	 * @brief Set the Right Encoder value.
+	 * 
+	 * @param value Value
+	 */
+	void SetRightEncoder(long value);
+
+	/**
+	 * @brief Get the Left Motor PWM value.
+	 * 
+	 * @return int16_t Value
+	 */
+	int16_t GetLeftMotor();
+
+	/**
+	 * @brief Get the Right Motor PWM value.
+	 * 
+	 * @return int16_t Value
+	 */
+	int16_t GetRightMotor();
+
+#pragma endregion
+
+};
+
+/** @brief Instance of the /h-Motor controller. */
+extern MotorControllerClass MotorController;
+
+#endif
+
