1728ff58ce28879bd5531ed71cbb63ba806365b9
Orlin Dimitrov <orlin369@gmail.com>
2023-10-23 23:34:35 +0300

    Beautifie the code and format

--- examples/us_servo/us_servo.ino
+++ examples/us_servo/us_servo.ino
@@ -1,9 +1,49 @@
-#define BLINK_INTERVAL 5000
+/*
+
+MIT License
+
+Copyright (c) [2023] [OpenMOBot]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#pragma region Definitions
+
+// #define DEBUG_TEXT
+
+#define DEBUG_OSC
+
+#define BLINK_INTERVAL 2000
+
+#pragma endregion
+
+#pragma region Headers
 
 #include <Servo.h>
 
 #include "OpenMOBot.h"
 #include "FxTimer.h"
+
+#pragma endregion
+
+#pragma region Variables
 
 /**
   * @brief Create servo object to control a servo.
@@ -34,6 +74,8 @@ FxTimer *BlinkTimer_g;
  * @brief Send timer instance.
  */
 FxTimer *ServoSweepTimer;
+
+#pragma endregion
 
 void setup()
 {
