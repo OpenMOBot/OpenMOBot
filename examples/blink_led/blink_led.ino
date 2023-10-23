d638d0cf49819c3b6d0b23da1a4e2d74eb9eafa8
Orlin Dimitrov <orlin369@gmail.com>
2023-10-23 23:33:12 +0300

    Beautifie the code and format

--- examples/blink_led/blink_led.ino
+++ examples/blink_led/blink_led.ino
@@ -1,14 +1,63 @@
+/*
+
+MIT License
+
+Copyright (c) [2023] [OpenMOBot]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#pragma region Definitions
+
+// #define DEBUG_TEXT
+
+#define DEBUG_OSC
+
 #define BLINK_INTERVAL 500
 
-#include "OpenMOBot.h"
+#pragma endregion
 
+#pragma region Headers
+
+#include "OpenMOBot.h"
 #include "FxTimer.h"
 
-// Variables will change:
-// StateStatusLED_g used to set the LED
-int StateStatusLED_g = LOW;  
+#ifdef DEBUG_TEXT
+#include "DebugPort.h"
+#endif
 
+#pragma endregion
+
+#pragma region Variables
+
+/**
+  * @brief StateStatusLED_g used to set the LED.
+  */
+int StateStatusLED_g = LOW;
+
+/** 
+ * @brief Blink timer instance.
+ */
 FxTimer *BlinkTimer_g;
+
+#pragma endregion
 
 void setup()
 {
@@ -34,4 +83,3 @@ void loop()
     digitalWrite(PIN_USER_LED, StateStatusLED_g);
   }
 }
-
