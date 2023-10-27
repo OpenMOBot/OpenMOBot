d93b2317865642dcb160451ffdf736e9e75e97a2
Orlin Dimitrov <orlin369@gmail.com>
2023-10-27 20:57:53 +0300

    Fix typos

--- src/LineSensor.h
+++ src/LineSensor.h
@@ -110,9 +110,9 @@ protected:
 
 	/** @brief Read a single sensor.
 	 *  @param int sensor, Sensor index.
-	 *  @return uint16_t, ADC filtred sensor value.
+	 *  @return uint16_t, ADC filtered sensor value.
 	 */
-	uint16_t readFiltredSensor(int sensorIndex);
+	uint16_t readFilteredSensor(int sensorIndex);
 
 #pragma endregion
 
@@ -157,7 +157,7 @@ public:
 
 	/** @brief read a single sensor.
 	 *  @param int sensor, Sensor index.
-	 *  @return uint16_t, ADC filtred sensor value.
+	 *  @return uint16_t, ADC filtered sensor value.
 	 */
 	void update();
 
@@ -171,9 +171,9 @@ public:
 	 */
 	float getLinePosition();
 
-	/** @brief Create histeresis binarization.
+	/** @brief Create hysteresis binarization.
 	 *  @param int sensor, Sensor index.
-	 *  @return bool, Threshhold level.
+	 *  @return bool, Threshold level.
 	 */
 	SensorState thresholdSensor(int sensorIndex);
 
