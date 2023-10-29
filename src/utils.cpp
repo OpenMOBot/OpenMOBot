f20b24d8b18fb3c9321f4dd5fd643d8ad73ba5db
Orlin Dimitrov <orlin369@gmail.com>
2023-10-29 18:37:25 +0200

    Add convertor from XY space to diferential LR space

---
+++ src/utils.cpp
@@ -0,0 +1,79 @@
+/*
+
+MIT License
+
+Copyright (c) [2023] [OpenMOBot]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+#include "utils.h"
+
+/** @brief Transform [X, Y] coordinates to [L, R] PWM values.
+ *  @param xyData X and Y "joystick" data.
+ *  @return LRData_t Left and Right PWM transformation values.
+ */
+LRData_t xy_to_lr(XYData_t xyData)
+{
+	static LRData_t LRDataL;
+
+	// Throttle (Y axis) and direction (X axis).
+	static int ThrottleL, DirectionL = 0;
+
+	// Left Motor helper variables.
+	int leftMotor;
+	float leftMotorScale = 0;
+
+	// Right Motor helper variables.
+	int rightMotor;
+	float rightMotorScale = 0;
+
+	// Holds the mixed output scaling factor.
+	float maxMotorScale = 0;
+
+	// Clear PWM data.
+	LRDataL.L = 0;
+	LRDataL.R = 0;
+
+	// Acquire the analog input for X and Y.
+	// Then rescale the 0..1023 range to -255..255 range.
+	ThrottleL = (512 - xyData.Y) / 2;
+	DirectionL = -(512 - xyData.X) / 2;
+
+	// Mix throttle and direction
+	leftMotor = ThrottleL + DirectionL;
+	rightMotor = ThrottleL - DirectionL;
+
+	// Calculate the scale of the results in comparision base 8 bit PWM resolution
+	leftMotorScale = leftMotor / 255.0;
+	leftMotorScale = abs(leftMotorScale);
+	rightMotorScale = rightMotor / 255.0;
+	rightMotorScale = abs(rightMotorScale);
+
+	// Choose the max scale value if it is above 1
+	maxMotorScale = max(leftMotorScale, rightMotorScale);
+	maxMotorScale = max(1, maxMotorScale);
+
+	//and apply it to the mixed values
+	LRDataL.L = constrain(leftMotor / maxMotorScale, -255, 255);
+	LRDataL.R = constrain(rightMotor / maxMotorScale, -255, 255);
+
+	return LRDataL;
+}
