4cf69ad03d4bfda332817d472090a85b5033649d
Kristiyan Gyorev <52297136+Chrischenitos@users.noreply.github.com>
2023-10-30 11:27:49 +0200

    Update MotorController.h
    Minor spelling mistakes in comments

--- src/MotorController.h
+++ src/MotorController.h
@@ -51,7 +51,7 @@ typedef struct
 /** @brief H-bridge motor Controller. */
 } MotorModel_t;
 
-/** @brief H-bridge motor hontroller. */
+/** @brief H-bridge motor Controller. */
 class MotorControllerClass
 {
 
